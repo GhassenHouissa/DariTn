0dcda954b06490ef150d88662cd551ded0e7be72
MSI <houissa123>
2021-03-03 18:40:02 +0100

    first commit

---
+++ DARI_TN/src/main/java/tn/dari/spring/DariTnApplication.java
@@ -0,0 +1,13 @@
+package tn.dari.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DariTnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DariTnApplication.class, args);
+	}
+
+}
