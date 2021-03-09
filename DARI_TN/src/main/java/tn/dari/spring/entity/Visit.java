fe6da73f1b49a8997bba734ec921692e20dd595c
Dell <Dell@DESKTOP-BLJM03O>
2021-03-09 15:00:25 +0100

    Commit aziz

--- DARI_TN/src/main/java/tn/dari/spring/entity/Visit.java
+++ DARI_TN/src/main/java/tn/dari/spring/entity/Visit.java
@@ -9,6 +9,23 @@ import javax.persistence.Table;
 @Entity
 @Table
 public class Visit {
+	
+	private String visite_date;
+
+	public Visit(String visite_date, Long idVisit, Ads ads) {
+		super();
+		this.visite_date = visite_date;
+		this.idVisit = idVisit;
+		this.ads = ads;
+	}
+
+	public String getVisite_date() {
+		return visite_date;
+	}
+
+	public void setVisite_date(String visite_date) {
+		this.visite_date = visite_date;
+	}
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
