0e6e48718e47f9014fa4e5dd45d8a5330561a9f5
MSI <houissa123>
2021-03-09 15:52:01 +0100

    visitand favorites

--- DARI_TN/src/main/java/tn/dari/spring/entity/User.java
+++ DARI_TN/src/main/java/tn/dari/spring/entity/User.java
@@ -14,53 +14,59 @@ import javax.persistence.Table;
 
 import net.bytebuddy.description.modifier.EnumerationState;
 
-
 @Entity
-@Table(name="T_USER")
+@Table(name = "T_USER")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-		@Id
-		@GeneratedValue (strategy = GenerationType.IDENTITY)
-		private Long Id; 
-		private String FirstName;
-		private String LastName;
-		private String Email;
-		public Long getId() {
-			return Id;
-		}
-		public void setId(Long id) {
-			Id = id;
-		}
-		public String getFirstName() {
-			return FirstName;
-		}
-		public void setFirstName(String firstName) {
-			FirstName = firstName;
-		}
-		public String getLastName() {
-			return LastName;
-		}
-		public void setLastName(String lastName) {
-			LastName = lastName;
-		}
-		public String getEmail() {
-			return Email;
-		}
-		public void setEmail(String email) {
-			Email = email;
-		}
-		
-		 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private String FirstName;
+	private String LastName;
+	private String Email;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getFirstName() {
+		return FirstName;
+	}
+
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
 	@OneToMany
-private List<Claim> claims;
-//	@OneToMany
-//	private List<Visit> visits;
-//	@OneToMany
-//	private List<Favorites> favorites;
+	private List<Claim> claims;
+	@OneToMany
+	private List<Visit> visits;
+	@OneToMany
+	private List<Favorites> favorites;
 	@OneToMany
 	private List<Bank> banks;
 	@OneToMany
-	private List<Historical>historicals;
+	private List<Historical> historicals;
 
 }
