f355f038cca7ddf8339f5066caf11f8dadf1ba49
MSI <houissa123>
2021-03-26 11:10:22 +0100

    finduserbyid

--- DARI_TN/src/main/java/tn/dari/spring/repository/UserRepository.java
+++ DARI_TN/src/main/java/tn/dari/spring/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package tn.dari.spring.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,5 +15,6 @@ public interface UserRepository extends CrudRepository<User ,Long>  {
 	@Query("FROM User u WHERE u.login = :login")
 	User findUserByLogin(@Param(value = "login") String login);
 	
-	
+	@Query("FROM User u WHERE u.id = :id")
+	List<User> findUserByID(@Param(value = "id") Long id);
 }
