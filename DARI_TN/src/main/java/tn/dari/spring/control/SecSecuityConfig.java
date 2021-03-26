4061ffbc871fd52a40f79f80b268a93c7fb51eab
MSI <houissa123>
2021-03-26 11:04:50 +0100

    mvc

--- DARI_TN/src/main/java/tn/dari/spring/control/SecSecuityConfig.java
+++ DARI_TN/src/main/java/tn/dari/spring/control/SecSecuityConfig.java
@@ -3,6 +3,7 @@ package tn.dari.spring.control;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import tn.dari.spring.entity.User;
 import tn.dari.spring.service.UserService;
 import tn.dari.spring.util.UserAuthenticationFailureHandler;
 import tn.dari.spring.util.UserLogoutSuccessHandler;
@@ -55,10 +55,23 @@ import tn.dari.spring.util.UserLogoutSuccessHandler;
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN").antMatchers("/anonymous*").anonymous().antMatchers("/login*").permitAll().anyRequest().authenticated().and().formLogin().defaultSuccessUrl("/success.html", true)
-					.and().httpBasic();
+//			http.cors().and().csrf().disable().authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN").antMatchers("/anonymous*").anonymous().antMatchers("/login*").permitAll().antMatchers(HttpMethod.POST, "/add-user").permitAll().anyRequest().authenticated().and().formLogin().defaultSuccessUrl("/success.html", true)
+//					.and().httpBasic();
 
-			
+			http.cors().and().csrf().disable()
+			.authorizeRequests()
+			.antMatchers(HttpMethod.DELETE,"/servlet/remove-user/{user-id}").permitAll()
+			.antMatchers(HttpMethod.PUT,"/servlet/modify-user").permitAll()
+			.antMatchers(HttpMethod.PUT,"/modify-user").permitAll()
+			.antMatchers(HttpMethod.POST,"/servlet/add-user").permitAll()
+			.antMatchers(HttpMethod.POST,"/add-user").permitAll()
+			.antMatchers(HttpMethod.GET,"/retrieve-all-users").permitAll()
+			.antMatchers(HttpMethod.GET,"/servlet/retrieve-all-users").permitAll()
+			.antMatchers(HttpMethod.GET,"/servlet/retrieve-all-users/{user-id}").permitAll()
+			.antMatchers(HttpMethod.GET,"/retrieve-all-users/{user-id}").permitAll()
+			.anyRequest().authenticated()
+			.and()
+            .httpBasic();
 	        
 		}
 		
--- DARI_TN/src/main/java/tn/dari/spring/repository/UserRepository.java
+++ DARI_TN/src/main/java/tn/dari/spring/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package tn.dari.spring.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -15,6 +13,5 @@ public interface UserRepository extends CrudRepository<User ,Long>  {
 	@Query("FROM User u WHERE u.login = :login")
 	User findUserByLogin(@Param(value = "login") String login);
 	
-	@Query("FROM User u WHERE u.id = :id")
-	List<User> findUserByID(@Param(value = "id") Long id);
+	
 }
